51926dc2665acb0cdea0e367ac40ea157364d8b7
Nikita Migushev <migushev@gmail.com>
2023-08-18 19:52:34 +0300

    0. Spring Security [#6879 #437558]

--- src/main/java/ru/job4j/accidents/configuration/SecurityConfig.java
+++ src/main/java/ru/job4j/accidents/configuration/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -11,6 +13,16 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    InMemoryUserDetailsManager users() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("John")
+                        .password("{noop}password")
+                        .roles("ADMIN")
+                        .build()
+        );
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
