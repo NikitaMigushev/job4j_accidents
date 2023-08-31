8ac1e3c13bfff8605cf23eb3359129642ccc235d
Nikita Migushev <migushev@gmail.com>
2023-08-31 15:23:02 +0300

    0. Spring test [#6881]

--- src/test/java/ru/job4j/accidents/controller/AccidentControllerTest.java
+++ src/test/java/ru/job4j/accidents/controller/AccidentControllerTest.java
@@ -4,10 +4,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.accidents.Main;
+import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.service.AccidentService;
 
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -16,9 +23,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
 class AccidentControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private AccidentService accidentService;
 
     @Test
     @WithMockUser
@@ -32,6 +43,10 @@ class AccidentControllerTest {
     @Test
     @WithMockUser
     public void shouldReturnEditPage() throws  Exception {
+        int accidentId = 1;
+        Accident mockAccident = new Accident();
+        mockAccident.setId(accidentId);
+        when(accidentService.findById(accidentId)).thenReturn(Optional.of(mockAccident));
         this.mockMvc.perform(get("/accident/edit")
                 .param("id", "1"))
                 .andDo(print())
